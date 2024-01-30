4221e24c5f5f5532f419f1334a5062499e220c1f
HallowNemesis <137962857+HallowNemesis@users.noreply.github.com>
2024-01-29 22:52:06 -0500

    added three new monsters with some game logic

--- js/platform_game.js
+++ js/platform_game.js
@@ -149,22 +149,30 @@ class Game{
         this.enemies = [];
         this.enemyInterval = 1000;
         this.enemyTimer = 0;
-        this.#addNewEnemy();
+        this.enemyTypes = ['worm', 'ghost', 'spider'];
     }
     update(deltaTime){
         this.enemies = this.enemies.filter(object => !object.markedForDeletion);
         if(this.enemyTimer > this.enemyInterval){
-            this.enemies.forEach(object => object.update());
+            this.#addNewEnemy();
             this.enemyTimer = 0;
         } else{
             this.enemyTimer += deltaTime;
         }
+        this.enemies.forEach(object => object.update(deltaTime));
     }
     draw(){
         this.enemies.forEach(object => object.draw(this.ctx));
     }
     #addNewEnemy(){
-        this.enemies.push(new Worm(this));
+        const randomEnemy = this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
+        if(randomEnemy == 'worm') this.enemies.push(new Worm(this));
+        if(randomEnemy == 'ghost') this.enemies.push(new Ghost(this));
+        if(randomEnemy == 'spider') this.enemies.push(new Spider(this));
+
+        this.enemies.sort(function(a,b){
+            return a.y - b.y;
+        });
     }
 }
 
@@ -172,18 +180,26 @@ class Game{
 class Enemy{
     constructor(game){
         this.game = game;
-        this.x = this.game.width;
-        this.y = Math.random() * this.game.height;
-        this.width = 100;
-        this.height = 100;
         this.markedForDeletion = false;
+        this.frameX = 0;
+        this.maxFrame = 5;
+        this.frameInterval = 100;
+        this.frameTimer = 0;
     }
-    update(){
-        this.x--;
+    update(deltaTime){
+        this.x -= this.vx * deltaTime;
         if(this.x < 0 - this.width) this.markedForDeletion = true;
+    
+        if(this.frameTimer > this.frameInterval){
+            if(this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+            this.frameTimer = 0;
+        } else{
+            this.frameTimer += deltaTime;
+        }
     }
     draw(ctx){
-        ctx.drawImage(this.image,0,0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image,this.frameX * this.spriteWidth,0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 
 };
@@ -193,13 +209,76 @@ class Worm extends Enemy{
         super(game);
         this.spriteWidth = 229;
         this.spriteHeight = 171;
-        this.width = 100;
-        this.height = 100;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
 
         this.x = this.game.width;
-        this.y = Math.random() * this.game.height;
+        this.y = this.game.height - this.height;
        
-        this.image = 'gameimgs/entities/raven.png';
+        this.image = 'gameimgs/entities/worm.png';
+        this.vx = Math.random() * 0.1 + 0.1;
+    }
+}
+
+class Ghost extends Enemy{
+    constructor(){
+        super(game);
+        this.spriteWidth = 261;
+        this.spriteHeight = 209;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height * 0.6;
+       
+        this.image = 'gameimgs/entities/ghost4.png';
+        this.vx = Math.random() * 0.2 + 0.1;
+        this.angle = 0;
+        this.curve = Math.random() * 3
+    }
+    update(deltaTime){
+        super.update(deltaTime);
+        this.y += Math.sin(this.angle) * this.curve;
+        this.angle += 0.04;
+    }
+    draw(ctx){
+        ctx.save();
+        ctx.globalAlpha = 0.5;
+        super.draw(ctx);
+        ctx.restore();
+
+    }
+}
+
+class Spider extends Enemy{
+    constructor(){
+        super(game);
+        this.spriteWidth = 310;
+        this.spriteHeight = 175;
+        this.width = this.spriteWidth/2;
+        this.height = this.spriteHeight/2;
+
+        this.x = Math.random() * this.game.width;
+        this.y = 0 - this.height;
+       
+        this.image = 'gameimgs/entities/small_spider.png';
+        this.vx = 0;
+        this.vy = Math.random() * 0.1 + 0.1;
+        this.maxLength = Math.random() * this.game.height;
+    }
+
+    update(deltaTime){
+        super.update(deltaTime);
+        if(this.y < 0 - this.width * 2) this.markedForDeletion = true;
+        this.y += this.vy * deltaTime;
+        if(this.y > this.maxLength) this.vy *= -1;
+    }
+    draw(ctx){
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width/2,0);
+        ctx.lineTo(this.x + this.width/2, this.y + 10);
+        ctx.stroke();
+        super.draw(ctx);
     }
 }
 
